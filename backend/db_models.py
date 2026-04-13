7625677ae9850fb612c27ba7c5f0a42a27d74d20
jun-3s <jaejun0602@naver.com>
2026-04-13 18:06:13 +0900

    db 모델 주석

--- backend/db_models.py
+++ backend/db_models.py
@@ -6,7 +6,7 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    userid = Column(String(20), unique=True, nullable=False, index=True)
-    password_hash = Column(String(255), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True) #고유번호 key
+    userid = Column(String(20), unique=True, nullable=False, index=True) #user 아이디
+    password_hash = Column(String(255), nullable=False) #user password 해싱
     created_at = Column(DateTime, server_default=func.now())
